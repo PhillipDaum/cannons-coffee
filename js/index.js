f3345577b4e7e4a87eac5a37916f4c468803dcb5
PhillipDaum <philgdaum@gmail.com>
2024-06-03 13:14:55 -0600

    begin integrating hamburger menu

---
+++ js/index.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector(".burger i");
+const nav = document.querySelector(".nav");
+
+function showHamburger() {
+  burger.classList.toggle("fa-times");
+  nav.classList.toggle("nav-active");
+}
